3e998ff19172863dc35ab1f2c728ac7000846353
Erik Smith <erik@cruiserhouse.com>
2015-06-10 16:29:45 -0700

    ODBC bindings

--- odbc/src/std/database/odbc/database.d
+++ odbc/src/std/database/odbc/database.d
@@ -1,55 +1,59 @@
 module std.database.odbc.database;
-//pragma(lib, "something");
+pragma(lib, "iodbc");
 
 import std.string;
 import std.c.stdlib;
 
-public import std.database.odbc.bindings;
+import std.database.odbc.sql;
+import std.database.odbc.sqlext;
+
 public import std.database.exception;
 
 import std.stdio;
 import std.typecons;
 
 struct Database {
+    public:
 
-    static Database create(string defaultURI) {
-        return Database(defaultURI);
-    }
-
-    private struct Payload {
-        string defaultURI;
-        void* env_;
-
-        this(string defaultURI_) {
-            defaultURI = defaultURI_;
-            writeln("odbc: opening database");
-            SQLRETURN ret;
-            ret = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env_);
-            //if (!SQL_SUCCEEDED(ret)) {
-            //throw std::runtime_error("error");
-            //}
+        static Database create(string defaultURI) {
+            return Database(defaultURI);
         }
 
-        ~this() {
-            writeln("odbc: closing database");
-            if (env_) {
-                SQLRETURN ret;
-                ret = SQLFreeHandle(SQL_HANDLE_ENV, env_);
+
+        this(string defaultURI) {
+            data_ = Data(defaultURI);
+        }
+
+    private:
+
+        struct Payload {
+            string defaultURI;
+            void* env_;
+
+            this(string defaultURI_) {
+                defaultURI = defaultURI_;
+                check("SQLAllocHandle", SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &env_));
+            }
+
+            ~this() {
+                writeln("odbc: closing database");
+                if (!env_) return;
+                check("SQLFreeHandle", SQLFreeHandle(SQL_HANDLE_ENV, env_));
                 env_ = null;
             }
+
+            this(this) { assert(false); }
+            void opAssign(Database.Payload rhs) { assert(false); }
         }
 
-        this(this) { assert(false); }
-        void opAssign(Database.Payload rhs) { assert(false); }
-    }
+        private alias RefCounted!(Payload, RefCountedAutoInitialize.no) Data;
+        private Data data_;
 
-    private alias RefCounted!(Payload, RefCountedAutoInitialize.no) Data;
-    private Data data_;
-
-    this(string defaultURI) {
-        data_ = Data(defaultURI);
-    }
-
+        static void check(string msg, SQLRETURN ret) {
+            writeln(msg);
+            if (ret == SQL_SUCCESS) return;
+            throw new DatabaseException("odbc error: " ~ msg);
+        }
 }
 
 
